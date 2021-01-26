4fb1440dbe4d809ff807de67c496eee2d5783d52
Aayush-Tadhani246 <70973409+Aayush-Tadhani246@users.noreply.github.com>
2021-01-25 19:56:44 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -19,7 +19,7 @@ function setup() {
   player.scale = 0.50
   fill("brown")
   ground = new Ground(500,600,1000,10)
-  rocks = new Rocks(random(100,900),0,250,200)
+ // rocks = new Rocks(random(100,900),0,250,200)
  
 }
 
@@ -28,7 +28,7 @@ function draw() {
   
   Engine.update(engine); 
   ground.display();
-  rocks.display();
+  // rocks.display();
   if(keyDown(RIGHT_ARROW)){
     if(player.x <900){
     player.x = player.x + 10
@@ -39,7 +39,36 @@ function draw() {
     player.x = player.x - 10
     }
   }
+
+  if(frameCount%60===0){
+    rocks.push(new Rocks(random(10, 990),0, 250,200));
+    
+  } 
+
+ for (var j = 0; j < rocks.length; j++) {
+  
+    rocks[j].display();
+  }
+
+  detectCollision(rocks,player);
+
+ 
   
   console.log(player.x)
   drawSprites()
+}
+
+function detectCollision(lrocks,lplayer){
+  // if(lplayer.collide(lrocks)){
+  //   text("GAME OVER",0,0,300,500)
+  // }
+  
+  //rocksBodyPosition = lrocks.body.position
+ // playerBodyPosition = lplayer.body.position
+
+  var distance = dist(lrocks.x,lrocks.y,lplayer.x,lplayer.y) 
+  if (distance<= lplayer.width + lrocks.width){  
+    text("GAME OVER",0,0,300,500)
+  
+  }
 }
